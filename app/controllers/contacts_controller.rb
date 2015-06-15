4c4c76f095bd5aa09f73474d4b3e0b1b8451f5c5
Duy <tranvuhoangduy@outlook.com>
2015-06-15 13:57:38 -0700

    add flash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,31 @@
 class ContactsController < ApplicationController
+
+	def index
+		@contacts = Contact.all
+	end
+
 	def new
 		@contact = Contact.new
 
 	end
 
 	def create
-		
+		@contact = Contact.new(contact_params)
+		if @contact.save
+			flash[:success] = "Message sent"
+			flash[:info] = "Message has not been sent"
+			flash[:warning] = "Message has not been sent"
+			flash[:danger] = "Message has not been sent"
+
+			redirect_to new_contact_path
+		else
+			flash[:danger] = "Message has not been sent"
+			redirect_to new_contact_path
+		end
+	end
+
+	private
+	def contact_params
+		params.require(:contact).permit(:name, :email , :comments)
 	end
 end
