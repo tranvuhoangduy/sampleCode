e310e2dfb1927d49a013b9812154918cd78c1242
Duy <tranvuhoangduy@outlook.com>
2015-06-16 01:26:56 -0700

    change gem file

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
