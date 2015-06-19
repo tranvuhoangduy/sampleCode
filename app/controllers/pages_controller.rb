d1507bc6c067c237d2ed1f0b2b56139d2ca92e79
Duy <tranvuhoangduy@outlook.com>
2015-06-19 13:39:04 -0700

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 	def index
-		@basic_plan = Plan.find(2)
-		@pro_plan = Plan.find(3)
+		@basic_plan = Plan.where(name:'basic')
+		@pro_plan = Plan.where(name:'pro')
 	end
 end
