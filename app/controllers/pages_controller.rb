997d7a851a5eb364834fcdedcfede59f84179554
Duy <tranvuhoangduy@outlook.com>
2015-06-26 09:10:06 -0700

    plan heroku id basic = 1 , pro=2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 	def index
-		@basic_plan = Plan.find(1)
-		@pro_plan = Plan.find(2)
+		@basic_plan = Plan.find_by_name('basic')
+		@pro_plan = Plan.find_by_name('pro')
 	end
 end
