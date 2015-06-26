747a8a12201155c34cae6a41903b177319199e4d
Duy <tranvuhoangduy@outlook.com>
2015-06-26 10:24:49 -0700

    f

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 		super do |resource|
 			if params[:plan]
 				resource.plan_id = params[:plan]
-				if resource.plan_id == '5' || resource.plan_id == "1"
+				if resource.plan_id == '5' || resource.plan_id == "2"
 					resource.save_with_payment
 				else
 					resource.save
